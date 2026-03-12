70e3f7afe473b377fd97c923d81dda70581333a7
Dennis Stumm <ds695sas@gmail.com>
2026-03-12 19:59:46 +0100

    Base embassy setup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 #![no_std]
 #![no_main]
 
-async fn main() -> ! {
+use embassy_executor::Spawner;
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+  embassy_stm32::init(Default::default());
+  defmt::info!("Hello World!");
 
   loop {}
 }
