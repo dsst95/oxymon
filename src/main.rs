b6f04b068e70fddc86da3da6d9425bdc7d35312f
Dennis Stumm <ds695sas@gmail.com>
2026-03-27 17:12:52 +0100

    Added components

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@ use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
+  // TODO: Configure the microcontroller's peripherals here.
+  // TODO: Initialize slint and render the splash screen using a state machine
   embassy_stm32::init(Default::default());
-  defmt::info!("Hello World!");
 
   loop {}
 }
